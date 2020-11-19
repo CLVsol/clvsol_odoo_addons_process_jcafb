627d2c28f45900a1c701b7f001220b16f1d2a218
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-11-19 18:59:17 -0300

    [IMP] clv_processing_jcafb (IMP models/reregistration_import_xls.py)

--- clv_processing_jcafb/models/reregistration_import_xls.py
+++ clv_processing_jcafb/models/reregistration_import_xls.py
@@ -6,6 +6,7 @@ import logging
 
 from functools import reduce
 from ast import literal_eval
+import xlrd
 
 from odoo import models
 
@@ -31,5 +32,30 @@ class AbstractProcess(models.AbstractModel):
         if schedule.method_args is not False:
             method_args = literal_eval(schedule.method_args)
         _logger.info(u'%s %s', '>>>>>>>>>> method_args: ', method_args)
+
+        filepath = method_args['file_path']
+        _logger.info(u'>>>>>>>>>> file_path: %s', filepath)
+        sheet_name = method_args['sheet_name']
+        _logger.info(u'>>>>>>>>>> sheet_name: %s', sheet_name)
+
+        book = xlrd.open_workbook(filepath)
+        sheet = book.sheet_by_name(sheet_name)
+
+        for i in range(sheet.nrows):
+
+            rec = sheet.cell_value(i, 0)
+            ok = sheet.cell_value(i, 1)
+            person_code = sheet.cell_value(i, 2)
+            name = sheet.cell_value(i, 3)
+            gender = sheet.cell_value(i, 4)
+            date_of_birth = sheet.cell_value(i, 5)
+            address = sheet.cell_value(i, 6)
+            district = sheet.cell_value(i, 7)
+            city = sheet.cell_value(i, 8)
+            responsible = sheet.cell_value(i, 9)
+
+            _logger.info(u'>>>>>>>>>> Ok: %s', ok)
+            if ok == 'x':
+                _logger.info(u'>>>>>>>>>>>>>>>> Name: %s', name)
 
         _logger.info(u'%s %s', '>>>>>>>> Execution time: ', secondsToStr(time() - start))
