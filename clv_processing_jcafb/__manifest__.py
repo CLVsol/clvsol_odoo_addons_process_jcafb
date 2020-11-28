ae0cb2067bfe3ee383c801fcfc88793d61814468
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-11-28 08:20:31 -0300

    [IMP] clv_processing_jcafb (REM family_address_restore)

--- clv_processing_jcafb/__manifest__.py
+++ clv_processing_jcafb/__manifest__.py
@@ -15,7 +15,6 @@
         # 'clv_base_jcafb',
     ],
     'data': [
-        # 'data/family_address_restore.xml',
         # 'data/person_family_restore.xml',
         # 'data/survey_user_input_copy.xml',
         'data/reregistration_import_xls.xml',
